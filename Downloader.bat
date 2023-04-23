cbdb1b7c4ee88a8ecdd8b7db6168350d9caf0530
VariousSoil1 <86133234+VariousSoil1@users.noreply.github.com>
2023-04-23 17:31:03 -0400

    Create Downloader.bat

---
+++ Downloader.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Please paste in your exploit path (example: C:\SynapseX)
+set /p executor=Path:
+echo Downloading...
+powershell -Command "Invoke-WebRequest https://github.com/VariousSoil1/DoorsV4/raw/main/vape.zip -OutFile vape.zip"
+echo Extracting...
+powershell -Command "Expand-Archive vape.zip -DestinationPath %executor%/workspace -Force"
+echo Deleting...
+del vape.zip
+echo Finished!
+pause
